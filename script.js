5a23526aea5af078e15a24b46ad900e47d9e924c
FADLI Saïd <saidfadli224@gmail.com>
2024-12-23 16:42:23 +0100

    Design v1

--- script.js
+++ script.js
@@ -5,23 +5,17 @@ fetch("movies.json")
     .then((movies) => {
         console.log(movies);
         movies.results.forEach(film => {
+            moment.locale("fr")
             document.getElementById("film").innerHTML += `
-            <div class="card p-0 col-lg-2 mb-3" style="width: 18rem;">
-                <div class="position-relative">
-                    <img src="${film.backdrop_path}"
-                        class="card-img-top" alt="${film.title}">
-                    <span class="d-inline-block position-absolute bottom-0 end-0 p-1" tabindex="0" data-bs-toggle="popover"
-                        data-bs-trigger="hover focus" data-bs-placement="top" data-bs-content="${film.vote_count} votants">
-                        <button class="btn btn-light" type="button" disabled> ${film.vote_average} /10</button>
-                    </span>
-                </div>
-                <div class="card-body h-auto">
-                    <h3 class="card-title fs-6">${film.title}</h3>
-                    <p class="text-secondary mb-0 h-auto"> ${film.release_date} </p>
-                </div>
-            </div>`
+            <div class="card position-relative p-0 mb-5 rounded">
+  <img src="${film.backdrop_path}" class="card-img-top" alt="Test">
+  <div class="card__content">
+    <p class="card__title m-0">${film.original_title} </p>
+    <p class="card__description mt-1">${film.overview} </p>
+    <p class="card__description mt-1">${moment(film.release_date).format("ll")} </p>
+  </div>
+</div>`
         });
+        const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+        const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
     })
-
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
