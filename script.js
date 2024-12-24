86b31da6e970a5c015e287e469ebc21e22466d2e
FADLI Saïd <saidfadli224@gmail.com>
2024-12-24 14:28:02 +0100

    ver mobil

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch("nextMovies.json")
         movies.results.forEach(film => {
             moment.locale("fr")
             document.getElementById("film").innerHTML += `
-            <div class="card position-relative p-0 mb-5 rounded">
+            <div class="card position-relative p-0 mb-lg-5 rounded">
   <img src="${film.backdrop_path}" class="card-img-top" alt="Test">
   <div class="card__content">
     <p class="card__title m-0">${film.original_title} </p>
