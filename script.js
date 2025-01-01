9e7ecceb55b994c192fde89c021577909ad2f981
FADLI Saïd <saidfadli224@gmail.com>
2025-01-01 21:30:37 +0100

    Change image

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ fetch("nextMovies.json")
       moment.locale("fr")
       document.getElementById("film").innerHTML += `
             <div class="card position-relative p-0 mb-lg-5 rounded">
-  <img src="${film.backdrop_path}" class="card-img-top" alt="Test">
+  <img src="https://image.tmdb.org/t/p/w500${film.poster_path}" class="card-img-top" alt="${film.original_title}">
   <div class="card__content">
     <p class="card__title m-0">${film.original_title} </p>
-    <p class="card__description mt-1">${film.overview} </p>
-    <p class="card__description mt-1">${moment(film.release_date).format("ll")} </p>
-    <span class="d-inline-block position-absolute bottom-0 end-0 p-1" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="${film.vote_count} votants">
+        <button class="card__description btn btn-light position-absolute bottom-0 start-0 p-2" type="button" disabled>${moment(film.release_date).format("ll")} </button>
+    <span class="d-inline-block m-0 position-absolute bottom-0 end-0 p-0" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="${film.vote_count} votants">
         <button class="btn btn-light" type="button" disabled>${film.vote_average} /10</button>
     </span>
   </div>
@@ -22,5 +21,13 @@ fetch("nextMovies.json")
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
-    document.getElementById("search").style.backgroundImage = `url("${movies.results[0].backdrop_path}")`;
+    document.getElementById("search").style.backgroundImage = `url("https://image.tmdb.org/t/p/original${movies.results[0].backdrop_path}")`;
   })
+
+
+
+  // Scroll horizontal possible avce la roulette de la sourie
+document.getElementById('film').addEventListener('wheel', (e) => {
+  e.preventDefault();
+  e.currentTarget.scrollLeft += e.deltaY;
+});
